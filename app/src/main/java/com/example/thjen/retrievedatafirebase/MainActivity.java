ecd147add866be74661b154555af88e6746f43ae
thjenit <thjenxxxno6@gmail.com>
2017-10-24 00:59:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/thjen/retrievedatafirebase/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.thjen.retrievedatafirebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference data;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView tv = (TextView) findViewById(R.id.tv);
+
+        data = FirebaseDatabase.getInstance().getReference();
+
+     //   data.child("Course").push().setValue("Unity");
+
+        data.child("Course").addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                tv.append(dataSnapshot.getValue().toString() + "\n");
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+
+}
